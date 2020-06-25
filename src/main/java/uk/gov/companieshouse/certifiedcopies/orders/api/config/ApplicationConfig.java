359de8133fd7d1dec1e3844b8690dba6c840f7c3
rjadhav <rjadhav@companieshouse.gov.uk>
2020-06-25 11:05:01 +0100

    Add authentication and authorisation interceptors to interceptor registry

--- src/main/java/uk/gov/companieshouse/certifiedcopies/orders/api/config/ApplicationConfig.java
+++ src/main/java/uk/gov/companieshouse/certifiedcopies/orders/api/config/ApplicationConfig.java
@@ -4,20 +4,29 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import uk.gov.companieshouse.certifiedcopies.orders.api.interceptor.LoggingInterceptor;
+import uk.gov.companieshouse.certifiedcopies.orders.api.interceptor.UserAuthenticationInterceptor;
+import uk.gov.companieshouse.certifiedcopies.orders.api.interceptor.UserAuthorisationInterceptor;
+import uk.gov.companieshouse.certifiedcopies.orders.api.service.CertifiedCopyItemService;
 
 import static com.fasterxml.jackson.databind.PropertyNamingStrategy.SNAKE_CASE;
 
 @Configuration
 public class ApplicationConfig implements WebMvcConfigurer {
 
+    @Autowired
+    private CertifiedCopyItemService certifiedCopyItemService;
+
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
         registry.addInterceptor(new LoggingInterceptor());
+        registry.addInterceptor(new UserAuthenticationInterceptor()).addPathPatterns("/orderable/**");
+        registry.addInterceptor(new UserAuthorisationInterceptor(certifiedCopyItemService)).addPathPatterns("/orderable/certified-copies/**");
     }
 
     @Bean
