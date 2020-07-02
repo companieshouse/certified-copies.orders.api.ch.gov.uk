4099e6972debb689042313b937d088bf081f8a03
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2020-07-02 08:06:38 +0100

    GCI-1209: Test code rationalisations.

--- src/test/java/uk/gov/companieshouse/certifiedcopies/orders/api/service/CompanyServiceIntegrationTest.java
+++ src/test/java/uk/gov/companieshouse/certifiedcopies/orders/api/service/CompanyServiceIntegrationTest.java
@@ -15,8 +15,8 @@ import org.springframework.core.env.Environment;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.server.ResponseStatusException;
 import uk.gov.companieshouse.api.model.company.CompanyProfileApi;
-
-import java.util.List;
+import uk.gov.companieshouse.certifiedcopies.orders.api.util.ApiErrorResponsePayload;
+import uk.gov.companieshouse.certifiedcopies.orders.api.util.Error;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.badRequest;
@@ -46,39 +46,8 @@ public class CompanyServiceIntegrationTest {
 
     private static final CompanyProfileApi COMPANY_PROFILE;
 
-    private static class Error {
-        private String type;
-        private String error;
-
-        private Error(String type, String error) {
-            this.type = type;
-            this.error = error;
-        }
-
-        public String getType() {
-            return type;
-        }
-
-        public String getError() {
-            return error;
-        }
-    }
-
-    private static class CompanyProfileApiErrorResponsePayload {
-
-        private List<Error> errors;
-
-        private CompanyProfileApiErrorResponsePayload(List<Error> errors) {
-            this.errors = errors;
-        }
-
-        public List<Error> getErrors() {
-            return errors;
-        }
-    }
-
-    private static final CompanyProfileApiErrorResponsePayload COMPANY_NOT_FOUND =
-            new CompanyProfileApiErrorResponsePayload(singletonList(new Error("ch:service", "company-profile-not-found")));
+    private static final ApiErrorResponsePayload COMPANY_NOT_FOUND =
+            new ApiErrorResponsePayload(singletonList(new Error("ch:service", "company-profile-not-found")));
 
     static {
         COMPANY_PROFILE = new CompanyProfileApi();
