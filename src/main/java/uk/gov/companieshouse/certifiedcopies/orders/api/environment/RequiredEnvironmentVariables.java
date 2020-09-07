04e3006d6b21d543ad9a3faae4241b4018899fbb
hwilliams1 <hwilliams1@companieshouse.gov.uk>
2020-09-07 07:29:28 +0100

    Added more required environment variables, updated unit tests and replaced tabs with spaces in test class

--- src/main/java/uk/gov/companieshouse/certifiedcopies/orders/api/environment/RequiredEnvironmentVariables.java
+++ src/main/java/uk/gov/companieshouse/certifiedcopies/orders/api/environment/RequiredEnvironmentVariables.java
@@ -3,7 +3,10 @@ package uk.gov.companieshouse.certifiedcopies.orders.api.environment;
 public enum RequiredEnvironmentVariables {
     
     ITEMS_DATABASE("ITEMS_DATABASE"),
-    MONGODB_URL("MONGODB_URL");
+    MONGODB_URL("MONGODB_URL"),
+    CHS_API_KEY("CHS_API_KEY"),
+    API_URL("API_URL"),
+    PAYMENTS_API_URL("PAYMENTS_API_URL");
     
     private String name;
     
