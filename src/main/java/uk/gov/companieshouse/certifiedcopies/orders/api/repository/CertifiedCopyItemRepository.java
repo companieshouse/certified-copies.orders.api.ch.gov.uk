953a5dbcdb7ee9cce7001fd8452d68b1abfc97e1
Rafa <rgarcia@companieshouse.gov.uk>
2020-11-18 16:21:16 +0000

    Remove Spring Data REST

--- src/main/java/uk/gov/companieshouse/certifiedcopies/orders/api/repository/CertifiedCopyItemRepository.java
+++ src/main/java/uk/gov/companieshouse/certifiedcopies/orders/api/repository/CertifiedCopyItemRepository.java
@@ -1,8 +1,9 @@
 package uk.gov.companieshouse.certifiedcopies.orders.api.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
+
 import uk.gov.companieshouse.certifiedcopies.orders.api.model.CertifiedCopyItem;
 
-@RepositoryRestResource
+@Repository
 public interface CertifiedCopyItemRepository extends MongoRepository<CertifiedCopyItem, String> { }
