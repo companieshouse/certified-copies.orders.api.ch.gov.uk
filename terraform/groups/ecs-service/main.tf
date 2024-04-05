f080a5dedbd3157270e5734c50f5673fbf673f97
rhilgenbergbastos-ch <rhilgenbergbastos@companieshouse.gov.uk>
2024-04-05 10:58:41 +0100

    chore: updated secrets module ref

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -93,7 +93,7 @@ module "ecs-service" {
   eric_secrets              = local.eric_secrets
 }
 module "secrets" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/secrets?ref=1.0.235"
+  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/secrets?ref=1.0.253"
 
   name_prefix = "${local.service_name}-${var.environment}"
   environment = var.environment
