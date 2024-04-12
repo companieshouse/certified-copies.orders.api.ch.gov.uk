f8a3a0d1e2673d9b6b15c2e883158ddef5ad476e
chillier-ch <chillier@companieshouse.gov.uk>
2024-04-12 12:18:33 +0100

    Update docker script permissions and fix env file name

--- terraform/groups/ecs-service/locals.tf
+++ terraform/groups/ecs-service/locals.tf
@@ -14,7 +14,7 @@ locals {
   healthcheck_matcher         = "200"
   vpc_name                    = local.stack_secrets["vpc_name"]
   s3_config_bucket            = data.vault_generic_secret.shared_s3.data["config_bucket_name"]
-  app_environment_filename    = "certified-copies-orders.api.ch.gov.uk.env"
+  app_environment_filename    = "certified-copies.orders.api.ch.gov.uk.env"
   use_set_environment_files   = var.use_set_environment_files
   application_subnet_ids      = data.aws_subnets.application.ids
 
